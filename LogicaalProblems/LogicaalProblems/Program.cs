66188c4d689579654650aa6d9d0635edb24a3a7b
Shreya Malviya <malviyashreya26@gmail.com>
2021-05-28 15:09:56 +0530

    [Shreya]Add: Calculating Fibonacci Series

--- LogicaalProblems/LogicaalProblems/Program.cs
+++ LogicaalProblems/LogicaalProblems/Program.cs
@@ -11,6 +11,21 @@ namespace LogicaalProblems
         static void Main(string[] args)
         {
             Console.WriteLine( "Welcome to Logical Problems ");
+            Console.WriteLine("Enter your choice ");
+            Console.WriteLine(".............................................");
+            Console.WriteLine("1 For: Fibonacci Series ");
+            int selection = Convert.ToInt32(Console.ReadLine());
+            switch (selection)
+            {
+                case 1:
+                    //Fibonacco Series
+                    fibonacciSeries FlibSeries = new fibonacciSeries();
+                    FlibSeries.CheckFibonacciSeries();
+                    break;
+                default:
+                    Console.WriteLine("Invalid Selection. Select 1");
+                    break;
+            }
             Console.ReadLine();
         }
     }
